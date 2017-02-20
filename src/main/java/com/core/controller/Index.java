436c56042b911037848123b2d51de8ad8aa9f9f0
sunwx <sunwx@esensoft.com>
2017-02-20 10:44:58 +0800

    整合freemaker到系统中

--- src/main/java/com/core/controller/Index.java
+++ src/main/java/com/core/controller/Index.java
@@ -1,13 +1,16 @@
 package com.core.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Index {
 
-	@RequestMapping("/index.shtml")
-	public String index(){
-		return "index";
+	@RequestMapping("/index.do")
+	public String index(ModelMap model){
+		model.addAttribute("ss", "sun111");
+		model.addAttribute("ssS", "sun11122");
+		return "NewFile";
 	}
 }
